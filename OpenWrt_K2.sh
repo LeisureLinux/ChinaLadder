379f22cdee032decf5d1fbafeb111f73aff4a4ea
Albert Xu <axu@yj777.cn>
2022-03-04 19:52:26 +0800

    fix url issue

--- OpenWrt_K2.sh
+++ OpenWrt_K2.sh
@@ -10,7 +10,7 @@
 # opkg update; opkg install libustream-openssl ca-bundle ca-certificates
 # 然后用 wget 调用本脚本
 # wget -4 --no-check-certificate -O -  \
-#    https://raw.githubusercontent.com/ZenBoy999/ChinaLadder/master/OpenWrt_K2.sh |sh
+#    https://raw.githubusercontent.com/LeisureLinux/ChinaLadder/master/OpenWrt_K2.sh |sh
 # 详细流程请参考：斐讯 K2 的不死鸟刷机-科学上网和广告屏蔽
 # URL: https://tech.yj777.cn/%e6%96%90%e8%ae%af-k2-%e7%9a%84%e4%b8%8d%e6%ad%bb%e9%b8%9f%e5%88%b7%e6%9c%ba-%e6%9c%80%e4%bd%b3%e5%ae%9e%e8%b7%b5/
 # QQ: 8122093
@@ -24,10 +24,10 @@ opkg -V0 install luci-i18n-base-zh-cn
 [ $? != 0 ] && echo "安装中文 Web 界面出错" && exit 2
 echo "安装 Polipo HTTP 代理中 ..."
 opkg -V0 install polipo luci-app-polipo luci-i18n-polipo-zh-cn
-[ $? != 0 ] && echo "安装 Polipo 出错"  && E=1
+[ $? != 0 ] && echo "安装 Polipo 出错" && E=1
 echo "安装 shadowsocks-libev-ss-local 中，请注意境外服务器端也要用 shadowsocks-libev，不能是 SSR ..."
 opkg -V0 install shadowsocks-libev-ss-local luci-app-shadowsocks-libev
-[ $? != 0 ] && echo "安装 shadowsocks-libev-local 出错"  && E=1
+[ $? != 0 ] && echo "安装 shadowsocks-libev-local 出错" && E=1
 echo "安装 DNSCrypt-Proxy DNS 加密，广告屏蔽中 ..."
 opkg -V0 install libustream-openssl dnscrypt-proxy luci-app-dnscrypt-proxy
 [ $? != 0 ] && echo "安装 DNS 加密出错" && E=1
@@ -43,7 +43,7 @@ uci set dhcp.@dnsmasq[0].nonwildcard='0'
 uci set dhcp.@dnsmasq[0].rebind_protection='0'
 uci commit
 echo " Github 上下载 wpad.dat 文件 ..."
-wget --no-check-certificate -O /www/wpad.dat "https://raw.githubusercontent.com/ZenBoy999/ChinaLadder/master/wpad.dat"
+wget --no-check-certificate -O /www/wpad.dat "https://raw.githubusercontent.com/LeisureLinux/ChinaLadder/master/wpad.dat"
 [ $? != 0 ] && echo "下载 wpad.dat 文件出错了" && E=1
 [ "$E" = "1" ] && echo "安装过程有出错的步骤，请手工检查，或者尝试重新运行本脚本"
 echo "接下来，您需要添加 shadowsocks-libev 的远程服务器，
